b26fde3f824ce29e3f3b27237593f498b688affd
erimsengezer <erimsengezer@gmail.com>
2019-06-27 16:57:28 +0300

    Initial Commit

---
+++ CatchKenny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatchKenny
+//
+//  Created by Erim Şengezer on 27.06.2019.
+//  Copyright © 2019 Erim Şengezer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
